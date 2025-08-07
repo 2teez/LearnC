3a8087169dd8384b33182c7e815c7110f507a4c7
timothy adigun <adigun.timothy@gmail.com>
2025-08-07 19:18:32 +0100

    practice string in c

---
+++ junkz/strings.c
@@ -0,0 +1,12 @@
+// strings.c
+
+// A complete C Program
+#include <stdio.h>
+#include "../str_util.h"
+
+int main(int argc, char** argv){
+
+    printf("[%s]\n", trim(" hello, world!    "));
+
+    return 0;
+}
