3037ee2ab313eed1699674f79d3c2790b170b668
timothy adigun <adigun.timothy@gmail.com>
2025-08-12 07:33:40 +0100

    local header folder for c devs

---
+++ includes/str_util.h
@@ -0,0 +1,13 @@
+#ifndef __STR_UTIL__
+#define __STR_UTIL__
+#
+#define SIZE 256
+
+void str_title(char*);
+char* trim(const char*);
+char* lstrip(const char*);
+char* rstrip(const char*);
+char* strip(const char*);
+char* input(const char*);
+
+#endif  /* __STR_UTIL__ */
