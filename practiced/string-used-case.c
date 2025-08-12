445557e9017225bb8a2f9e305647ab6a99a97c4f
timothy adigun <adigun.timothy@gmail.com>
2025-08-12 08:26:20 +0100

    c string tester

---
+++ practiced/string-used-case.c
@@ -0,0 +1,13 @@
+// string-used-case.c
+
+// A complete C Program
+#include <stdio.h>
+#include "../includes/str_util.h"
+
+int main(int argc, char** argv){
+
+    char* name = input("Enter your name: ");
+
+    printf("My name is: %s\n", name);
+    return 0;
+}
