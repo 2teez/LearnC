12ea0b9206bd6a7963d1625880fc9c0b5c5fbe52
timothy adigun <adigun.timothy@gmail.com>
2026-06-02 14:57:41 +0100

    c program

---
+++ modernC/ch00/src/getting-started2.c
@@ -0,0 +1,12 @@
+/* getting-started2.c */
+
+/* A complete C Program */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, [[maybe_unused]] char* argv[argc+1]){
+
+    printf("Hello, World!\n");
+
+    return EXIT_SUCCESS;
+}
